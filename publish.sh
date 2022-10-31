0daf6cbca33bf5d5de89b54dbacfbbcbd1c8daf4
Frank Boës <fboes@3960.org>
2022-10-31 08:02:52 +0100

    Changed deployment script

--- publish.sh
+++ publish.sh
@@ -5,9 +5,9 @@ echo "Latest tag: "
 git describe --tags
 read -p 'Enter new semver string (e.g. "1.0.0"): ' VERSION
 
-git merge master
+git merge main
 git push
-git checkout master
+git checkout main
 git merge develop
 git tag -a "v${VERSION}" -m "${VERSION}"
 git push && git push --tag
